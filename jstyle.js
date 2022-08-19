0437df7e9ea57c3fee5dda78228cac8b8fe436ba
hammadansari567 <hammadansari567@gmail.com>
2022-08-19 10:22:56 +0530

    change

--- jstyle.js
+++ jstyle.js
@@ -4,9 +4,9 @@ document.querySelector('#menu-btn').onclick = () =>{
   navbar.classList.toggle('active');
 }
 
-document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn => {
-  btn.onclick = () => {
-      let src = btn.getAttribute('data-src');
-      document.querySelector('.about .video-container .video').src = src;
-  }
-})
+// document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn => {
+//   btn.onclick = () => {
+//       let src = btn.getAttribute('data-src');
+//       document.querySelector('.about .video-container .video').src = src;
+//   }
+// })
